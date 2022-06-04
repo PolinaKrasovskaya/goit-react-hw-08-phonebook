0a43fb2bbd91847b2965b353c1d75ae1981e709a
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2022-06-04 14:27:14 +0100

    Updates

---
+++ static/js/919.bdba7bc1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[919],{919:function(n,e,h){h.r(e),h.d(e,{BooksView:function(){return o}});var r=h(184),o=function(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)("p",{children:"\u041a\u043d\u0438\u0433\u0438"}),(0,r.jsx)("p",{children:"fgnhrhghndgth"})]})}}}]);
+//# sourceMappingURL=919.bdba7bc1.chunk.js.map
